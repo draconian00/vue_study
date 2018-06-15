e3ab6a99acc3d7eae482a93bd0e36953a7714dcf
draconian00 <draconian00@gmail.com>
2018-06-15 13:21:25 +0900

    11.5 fix

--- todolistapp/src/store/actions.js
+++ todolistapp/src/store/actions.js
@@ -1,18 +1,16 @@
 import Constant from '../constant';
 
 export default {
-  actions: {
-    [Constant.ADD_TODO]: (store, payload) => {
-        console.log('### addTodo!!!');
-        store.commit(Constant.ADD_TODO, payload);
-    },
-    [Constant.DELETE_TODO]: (store, payload) => {
-        console.log('### deleteTodo!!!');
-        store.commit(Constant.DELETE_TODO, payload);
-    },
-    [Constant.DONE_TOGGLE]: (store, payload) => {
-        console.log('### doneToggle!!!');
-        store.commit(Constant.DONE_TOGGLE, payload);
-    }
+  [Constant.ADD_TODO]: (store, payload) => {
+      console.log('### addTodo!!!');
+      store.commit(Constant.ADD_TODO, payload);
+  },
+  [Constant.DELETE_TODO]: (store, payload) => {
+      console.log('### deleteTodo!!!');
+      store.commit(Constant.DELETE_TODO, payload);
+  },
+  [Constant.DONE_TOGGLE]: (store, payload) => {
+      console.log('### doneToggle!!!');
+      store.commit(Constant.DONE_TOGGLE, payload);
   },
 };
